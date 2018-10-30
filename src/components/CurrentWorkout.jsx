41ea0ec6fbeb1feb628544657853bc51706c86f1
Mariusz <siewruk.mariusz92@gmail.com>
2018-10-30 10:49:28 +0100

    fixed problem with git pages

---
+++ src/components/CurrentWorkout.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class CurrentWorkout extends Component {
+    render() {
+        return (
+            <div className="section__main">
+                <div className="section__main">
+                    <div className="progress-wrapper">
+                        <h2>In progress</h2>
+                        <div className="spinner-animation"><i className="fas fa-spinner"></i></div>
+                        <h3>This feature will be ready soon.</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CurrentWorkout
